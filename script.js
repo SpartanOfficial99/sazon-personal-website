f84981a08c3f8336c15697f25c859085cf2484a4
SpartanOfficial99 <untildawn2666@gmail.com>
2024-12-07 21:40:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function boxClicked(feature) {
     const output = document.getElementById('output');
-    output.textContent = You clicked on "${feature}"!;
+    output.textContent = `You clicked on "${feature}"!`;
     output.style.color = "#ffd700"; // Highlight the message
     output.style.animation = "pulse 1s infinite"; // Add an animation to the message
-} 
+}
+
+
